87c058a09b4ca38a85e63e001e7c1f8363b33909
Josef Kolář <thejoeejoee@gmail.com>
2022-01-31 14:15:04 +0100

    Typing tweaks

--- fiesta/apps/sections/middleware/user_membership.py
+++ fiesta/apps/sections/middleware/user_membership.py
@@ -1,16 +1,21 @@
 from __future__ import annotations
 
 from django.contrib.auth.models import AnonymousUser
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest as DjHttpRequest, HttpResponse
 
 from ...accounts.models import User
+from ..models import SectionMembership
+
+
+class HttpRequest(DjHttpRequest):
+    membership: SectionMembership | None
 
 
 class UserMembershipMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
-    def __call__(self, request: HttpRequest) -> HttpResponse:
+    def __call__(self, request: DjHttpRequest) -> HttpResponse:
         user: User | AnonymousUser = request.user
 
         if user.is_authenticated:
@@ -25,4 +30,4 @@ class UserMembershipMiddleware:
         return response
 
 
-__all__ = ["UserMembershipMiddleware"]
+__all__ = ["UserMembershipMiddleware", "HttpRequest"]
