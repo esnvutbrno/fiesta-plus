776c8266ed4c80307a30e10f000d2e08f53aac7e
Josef Kolář <thejoeejoee@gmail.com>
2022-02-17 10:25:05 +0100

    Fixed never matching regex for development django staticfiles, tweaked django checks

--- fiesta/fiesta/checks/models.py
+++ fiesta/fiesta/checks/models.py
@@ -17,7 +17,6 @@ import inspect
 
 import django.apps
 import django.core.checks
-
 from django.core.exceptions import FieldDoesNotExist
 
 
@@ -26,6 +25,13 @@ def get_argument(node, arg):
         if kw.arg == arg:
             return kw
     return None
+
+
+def is_constant_node(node):
+    if not isinstance(node, ast.Name):
+        return False
+
+    return node.id.replace('_', '').isupper()
 
 
 def is_gettext_node(node):
@@ -90,8 +96,8 @@ def check_model(model):
                 else:
                     value = verbose_name.value.args[0].s  # type: ignore
                     if not all(
-                        w.islower() or w.isupper() or w.isdigit()
-                        for w in value.split(" ")
+                            w.islower() or w.isupper() or w.isdigit()
+                            for w in value.split(" ")
                     ):
                         yield django.core.checks.Warning(
                             "Words in verbose name must be all upper case or all lower case",
@@ -102,7 +108,7 @@ def check_model(model):
 
             help_text = get_argument(node, "help_text")
             if help_text is not None:
-                if not is_gettext_node(help_text.value):
+                if not is_gettext_node(help_text.value) and not is_constant_node(help_text.value):
                     yield django.core.checks.Warning(
                         "Help text should use gettext",
                         hint="Use gettext on the help text.",
@@ -116,7 +122,7 @@ def check_model(model):
                     yield django.core.checks.Warning(
                         "Must set db_index explicitly on a ForeignKey field",
                         hint="Set db_index on the field ("
-                        "https://hakibenita.com/9-django-tips-for-working-with-databases#fk-indexes).",
+                             "https://hakibenita.com/9-django-tips-for-working-with-databases#fk-indexes).",
                         obj=field,
                         id="H008",
                     )
