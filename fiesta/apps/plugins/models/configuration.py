380938acfbd35207cec59f3080ddd8331b13de52
Josef Kolář <thejoeejoee@gmail.com>
2022-02-18 15:02:56 +0100

    BasePluginConfiguration cannot be instantiated directly

--- fiesta/apps/plugins/models/configuration.py
+++ fiesta/apps/plugins/models/configuration.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django_lifecycle import LifecycleModelMixin
@@ -10,7 +11,7 @@ class BasePluginConfiguration(LifecycleModelMixin, BasePolymorphicModel):
     Base configuration model for plugins.
     Usually it's extended and used for configuring running plugin.
 
-    Cannot be abstract, since `Plugin.configuration` is pointing at it.
+    Unfortunately cannot be abstract, since `Plugin.configuration` is pointing at it.
     """
 
     name = models.CharField(
@@ -25,6 +26,11 @@ class BasePluginConfiguration(LifecycleModelMixin, BasePolymorphicModel):
 
     def __str__(self):
         return f"{self.polymorphic_ctype.name if self.polymorphic_ctype else '---'}: {self.name}"
+
+    def clean(self):
+        # cannot use isinstance, since children are allowed to create
+        if type(self) == BasePluginConfiguration:
+            raise ValidationError(_('Base plugin configuration cannot be saved directly, only children.'))
 
 
 __all__ = ["BasePluginConfiguration"]
