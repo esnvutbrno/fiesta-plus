ffd176d0bc93b6e200a429fadbc8d157b6be2a50
Josef Kolář <thejoeejoee@gmail.com>
2022-07-23 19:46:27 +0200

    Added accounts index view

--- fiesta/apps/accounts/urls.py
+++ fiesta/apps/accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
+from apps.accounts.views.index import IndexView
 from apps.accounts.views.members import SectionMembersView
 from apps.accounts.views.membership import MembershipView, NewMembershipFormView
 from apps.accounts.views.profile import MyProfileView, ProfileFinishFormView
 
 urlpatterns = [
+    path("", IndexView.as_view(), name="index"),
     path("profile/finish", ProfileFinishFormView.as_view(), name="profile-finish"),
     path("profile", MyProfileView.as_view(), name="profile"),
     path("membership/new", NewMembershipFormView.as_view(), name="membership-new"),
