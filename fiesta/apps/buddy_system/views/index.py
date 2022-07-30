31f5abe3da0594a7e52af08eb328c996f99584e9
Josef Kolář <thejoeejoee@gmail.com>
2022-07-30 05:03:11 +0200

    buddy system: breadcrumbs improved

--- fiesta/apps/buddy_system/views/index.py
+++ fiesta/apps/buddy_system/views/index.py
@@ -1,11 +1,14 @@
+from django.utils.translation import gettext_lazy as _
 from django.views.generic import TemplateView
 
 from apps.buddy_system.models import BuddySystemConfiguration
 from apps.plugins.views import PluginConfigurationViewMixin
 from apps.sections.middleware.user_membership import HttpRequest
 from apps.sections.models import SectionMembership
+from apps.utils.breadcrumbs import with_breadcrumb
 
 
+@with_breadcrumb(_("Buddy System"))
 class BuddySystemIndexView(
     PluginConfigurationViewMixin[BuddySystemConfiguration], TemplateView
 ):
