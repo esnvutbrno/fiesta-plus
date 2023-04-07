3574768a8c6ece42c45846d75c8167446656f6bf
Josef Kolář <josef.kolar@firma.seznam.cz>
2023-04-07 17:10:56 +0200

    feat(esncard): partners importer WIP

---
+++ fiesta/apps/esncards/management/commands/import_esncard_partners.py
@@ -0,0 +1,10 @@
+import djclick as click
+
+
+@click.command()
+def sync():
+    from apps.esncards.services.esncard_partners_importer import EsnCardPartnersImporter
+
+    importer = EsnCardPartnersImporter()
+
+    importer.sync()
---
+++ fiesta/apps/esncards/services/esncard_partners_importer.py
@@ -0,0 +1,31 @@
+import logging
+
+import requests
+from bs4 import BeautifulSoup
+
+logger = logging.getLogger(__name__)
+
+
+class EsnCardPartnersImporter:
+    API_URL = "https://esncard.org/discover/list?query=&page={page}"
+
+    def sync(self):
+        page = 1
+
+        while True:
+            response = requests.get(self.API_URL.format(page=page))
+
+            if response.status_code == 400:
+                break
+
+            markup = BeautifulSoup(response.text, "html.parser")
+
+            self._sync_from_page(page=markup)
+
+            page += 1
+
+    def _sync_from_page(self, page: BeautifulSoup):
+        for partner in page.select(".discount-list-item"):
+            title = partner.select_one("h3").text
+
+            logger.info(title)
