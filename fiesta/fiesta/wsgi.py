8da73b82586d15408bc8179240d108c9fe0bc314
Josef Kolář <josef.kolar@firma.seznam.cz>
2023-04-29 16:28:30 +0200

    feat(ci): fix django settings module

--- fiesta/fiesta/wsgi.py
+++ fiesta/fiesta/wsgi.py
@@ -8,11 +8,6 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 from __future__ import annotations
 
-import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fiesta.settings")
-os.environ.setdefault("DJANGO_CONFIGURATION", "Development")
 
 application = get_wsgi_application()
--- nginx/webserver.Dockerfile
+++ nginx/webserver.Dockerfile
@@ -1,6 +1,10 @@
+# syntax = edrevo/dockerfile-plus
+
 FROM nginx:1.21.5-alpine
 
 RUN rm /etc/nginx/conf.d/default.conf
+
+INCLUDE+ Dockerfile.common
 
 # https://github.com/docker-library/docs/tree/master/nginx#using-environment-variables-in-nginx-configuration-new-in-119
 COPY ./nginx.conf.template /etc/nginx/templates/
