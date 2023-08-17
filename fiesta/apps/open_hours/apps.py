e93b68a84685957bb2c3c870767b576aed6a94c5
Josef Kolář <josef.kolar@firma.seznam.cz>
2023-08-17 14:48:32 +0200

    feat(oh): restrict map view based on configuration

--- fiesta/apps/open_hours/apps.py
+++ fiesta/apps/open_hours/apps.py
@@ -1,8 +1,17 @@
 from __future__ import annotations
 
+import typing
+
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 from apps.plugins.plugin import BasePluginAppConfig
+from apps.utils.templatetags.navigation import NavigationItemSpec
+
+if typing.TYPE_CHECKING:
+    from apps.open_hours.models import OpenHoursConfiguration
+    from apps.plugins.middleware.plugin import HttpRequest
+    from apps.plugins.models.plugin import Plugin
 
 
 class OpenHoursConfig(BasePluginAppConfig):
@@ -13,6 +22,20 @@ class OpenHoursConfig(BasePluginAppConfig):
     description = _("Open Hours shows the open hours of your office place.")
 
     configuration_model = "open_hours.OpenHoursConfiguration"
+
+    def as_navigation_item(self, request: HttpRequest, bound_plugin: Plugin) -> NavigationItemSpec | None:
+        base = super().as_navigation_item(request, bound_plugin)
+        cfg: OpenHoursConfiguration = bound_plugin.configuration
+
+        if not cfg.show_map:
+            return base
+
+        return base._replace(
+            # url="",
+            children=[
+                NavigationItemSpec(title=_("Map"), url=reverse("open_hours:map")),
+            ],
+        )
 
 
 __all__ = ["OpenHoursConfig"]
--- fiesta/apps/open_hours/views/index.py
+++ fiesta/apps/open_hours/views/index.py
@@ -1,10 +1,23 @@
 from __future__ import annotations
+
+from django.http import Http404
 from django.views.generic import TemplateView
+
+from apps.plugins.views import PluginConfigurationViewMixin
 
 
 class OpenHoursIndexView(TemplateView):
     template_name = "open_hours/index.html"
 
 
-class MapView(TemplateView):
+class MapView(
+    PluginConfigurationViewMixin,
+    TemplateView,
+):
     template_name = "open_hours/map.html"
+
+    def get(self, request, *args, **kwargs):
+        if not self.configuration.show_map:
+            raise Http404("Map is not enabled")
+
+        return super().get(request, *args, **kwargs)
