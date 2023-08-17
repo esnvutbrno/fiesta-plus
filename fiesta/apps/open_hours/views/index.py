0f5949010b6111043a47fb6399deee419e0afbb8
Josef Kolář <josef.kolar@firma.seznam.cz>
2023-08-17 15:26:38 +0200

    feat(oh): display open hours

--- fiesta/apps/open_hours/views/index.py
+++ fiesta/apps/open_hours/views/index.py
@@ -1,13 +1,27 @@
 from __future__ import annotations
 
+import itertools
+
 from django.http import Http404
 from django.views.generic import TemplateView
 
 from apps.plugins.views import PluginConfigurationViewMixin
+from apps.sections.views.mixins.section_space import EnsureInSectionSpaceViewMixin
 
 
-class OpenHoursIndexView(TemplateView):
+class OpenHoursIndexView(EnsureInSectionSpaceViewMixin, TemplateView):
     template_name = "open_hours/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        groups = itertools.groupby(
+            self.request.in_space_of_section.open_hours.all(),
+            lambda x: x.day_index,
+        )
+
+        context["open_hours"] = [(day_index, list(group)) for day_index, group in groups]
+        return context
 
 
 class MapView(
