2483c4e89417673f45efb0cdc028c549b550550c
Josef Kolář <josef.kolar@firma.seznam.cz>
2023-08-17 16:02:00 +0200

    feat(oh): allow upload of custom maps

--- fiesta/apps/open_hours/models/configuration.py
+++ fiesta/apps/open_hours/models/configuration.py
@@ -3,13 +3,34 @@ from __future__ import annotations
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from apps.files.storage import NamespacedFilesStorage
 from apps.plugins.models import BasePluginConfiguration
+from apps.sections.middleware.user_membership import HttpRequest
+
+
+def has_perms_to_view_map(request: HttpRequest, name: str) -> bool:
+    # TODO: check if the configuration allows the access to the map
+    return True
+
+
+map_storage = NamespacedFilesStorage(
+    "open-hours-map",
+    has_permission=has_perms_to_view_map,
+)
 
 
 class OpenHoursConfiguration(BasePluginConfiguration):
     show_map = models.BooleanField(
         verbose_name=_("show map on open hours page"),
         default=True,
+    )
+
+    map = models.ImageField(
+        verbose_name=_("map"),
+        upload_to=map_storage.upload_to,
+        storage=map_storage,
+        blank=True,
+        null=True,
     )
 
     class Meta:
