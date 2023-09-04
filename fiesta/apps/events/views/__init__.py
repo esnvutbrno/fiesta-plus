5eb51dc27ec60d38aeb93c94912f82ebc07aaf3d
Petr Češka <ceskape@seznam.cz>
2023-09-04 11:45:11 +0200

    events base structura and first DB objects

---
+++ fiesta/apps/events/views/__init__.py
@@ -0,0 +1,5 @@
+from .index import EventsIndexView
+
+__all__ = [
+    "EventsIndexView"
+]
---
+++ fiesta/apps/events/views/index.py
@@ -0,0 +1,5 @@
+from django.views.generic import TemplateView
+
+
+class EventsIndexView(TemplateView):
+    template_name = 'events/index.html'
