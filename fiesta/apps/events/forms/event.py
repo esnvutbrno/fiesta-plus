48df26f0035f7c917c456fc908358d97f8fd345b
petrceska <ceskape@seznam.cz>
2023-10-05 14:07:07 +0200

    forms

--- fiesta/apps/events/forms/event.py
+++ fiesta/apps/events/forms/event.py
@@ -3,6 +3,9 @@ from apps.events.models import Event, Organizer
 from django.forms import Field as FormField, modelform_factory, CharField, HiddenInput, ChoiceField
 from django.utils.translation import gettext_lazy as _
 
+from apps.sections.models import SectionMembership
+
+
 class AddEventForm(BaseModelForm):
     section_name = CharField(label=_("ESN section"), disabled=True)
     author_name = CharField(label=_("Author of the event"), disabled=True)
@@ -41,8 +44,10 @@ class AddEventForm(BaseModelForm):
 class UpdateEventForm(BaseModelForm):
     # section_name = CharField(label=_("ESN section"), disabled=True)
     # author_name = CharField(label=_("Author of the event"), disabled=True)
+    memberships_queryset = SectionMembership.objects.all()
+    membership_choices = [(membership.user.id, membership.user.full_name_official) for membership in memberships_queryset]
 
-    add_organizer = ChoiceField()
+    add_organizer = ChoiceField(choices=membership_choices, label=_("Add organizer"), required=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
