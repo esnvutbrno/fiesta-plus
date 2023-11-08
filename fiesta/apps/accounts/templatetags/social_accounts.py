060b0b4910028e365335e6dfee12174f29c23299
Josef Kolář <josef.kolar@firma.seznam.cz>
2023-11-08 20:12:20 +0100

    feat(social-accounts): show only providers with active SocialApp

--- fiesta/apps/accounts/templatetags/social_accounts.py
+++ fiesta/apps/accounts/templatetags/social_accounts.py
@@ -1,8 +1,28 @@
 from __future__ import annotations
 
+from operator import attrgetter
+
+from allauth.socialaccount import providers
+from allauth.socialaccount.models import SocialApp
+from allauth.socialaccount.providers.base import Provider
 from django import template
+from django.contrib.sites.models import Site
 
 register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def get_all_available_social_providers(context) -> list[Provider]:
+    request = context["request"]
+    all_providers = providers.registry.get_list()
+
+    site = Site.objects.get_current(request=request)
+
+    all_available = SocialApp.objects.filter(
+        sites__id=site.id, provider__in=tuple(map(attrgetter("id"), all_providers))
+    ).values_list("provider", flat=True)
+
+    return [p for p in all_providers if p.id in all_available]
 
 
 @register.filter
