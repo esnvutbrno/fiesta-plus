9dd6ca5ccc594d26fbf0fe237c2ed2d96fab1596
petrceska <ceskape@seznam.cz>
2023-11-09 16:13:25 +0100

    ¨ getting registered events

--- fiesta/apps/events/views/index.py
+++ fiesta/apps/events/views/index.py
@@ -1,7 +1,23 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from apps.events.models import Event
+from apps.events.models.participant import ParticipantState
 
 
-class EventsIndexView(TemplateView):
+class EventsIndexView(ListView):
     template_name = 'events/index.html'
+    model = Event
 
-    
+    def get_queryset(self):
+        return super().get_queryset()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Get the user's confirmed participants
+        participating = self.request.user.participants.filter(state=ParticipantState.CONFIRMED)
+
+        # Add the confirmed participants to the context
+        context['participating'] = participating
+
+        return context
