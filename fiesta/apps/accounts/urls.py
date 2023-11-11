ae889093807fff4c80420379400f948b817dc8c8
Josef Kolář <josef.kolar@firma.seznam.cz>
2023-11-11 12:17:43 +0100

    feat(memberships): added smart after-login router

--- fiesta/apps/accounts/urls.py
+++ fiesta/apps/accounts/urls.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from django.urls import path
 from django.views.generic import RedirectView
 
+from apps.accounts.views.index import AfterLoginRedirectView
 from apps.accounts.views.membership import MyMembershipsView, NewSectionMembershipFormView
 from apps.accounts.views.profile import MyProfileDetailView, MyProfileUpdateView, ProfileFinishFormView
 
@@ -21,4 +22,5 @@ urlpatterns = [
     path("membership", MyMembershipsView.as_view(), name="membership"),
     # path("", TemplateView.as_view(template_name="accounts/index.html")),
     # path("login", LoginViewNotInSectionSpace.as_view(), name="login"),
+    path("after-login", AfterLoginRedirectView.as_view(), name="after-login"),
 ]
--- fiesta/apps/accounts/views/index.py
+++ fiesta/apps/accounts/views/index.py
@@ -1,12 +1,24 @@
 from __future__ import annotations
 
-# class BuddySystemEntrance(EnsureInSectionSpaceViewMixin, PluginConfigurationViewMixin, TemplateView):
-#     def get(self, request, *args, **kwargs):
-#         if self.request.membership.is_international:
-#             return HttpResponseRedirect(reverse("buddy_system:new-request"))
-#
-#         c: BuddySystemConfiguration = self.configuration
-#         if c.matching_policy_instance.can_member_match:
-#             return HttpResponseRedirect(reverse("buddy_system:matching-requests"))
-#
-#         return HttpResponseRedirect(reverse("buddy_system:index"))
+from django.urls import reverse
+from django.views.generic import RedirectView
+
+from apps.accounts.templatetags.memberships import section_membership_activation_url
+from apps.plugins.views import PluginConfigurationViewMixin
+from apps.sections.models import SectionMembership
+from apps.sections.views.mixins.section_space import EnsureNotInSectionSpaceViewMixin
+
+
+class AfterLoginRedirectView(
+    EnsureNotInSectionSpaceViewMixin,
+    PluginConfigurationViewMixin,
+    RedirectView,
+):
+    def get_redirect_url(self):
+        active_memberships = self.request.all_memberships.filter(state=SectionMembership.State.ACTIVE)
+        if active_memberships.count() == 1:
+            m: SectionMembership = active_memberships.first()
+
+            return section_membership_activation_url(dict(request=self.request), m)
+
+        return reverse("accounts:membership")
