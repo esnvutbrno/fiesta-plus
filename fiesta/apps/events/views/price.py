9d9dc0c9b0e77abf2c291fa7fcf0734286c3e36c
Daniel Němec <deawer23@seznam.cz>
2023-11-14 20:33:29 +0100

    Price button modal need fix

--- fiesta/apps/events/views/price.py
+++ fiesta/apps/events/views/price.py
@@ -27,6 +27,7 @@ from ...fiestatables.views.tables import FiestaTableView
 from ...sections.views.mixins.membership import EnsurePrivilegedUserViewMixin
 from ...utils.breadcrumbs import with_breadcrumb
 from allauth.account.utils import get_next_redirect_url
+from django.db import transaction
 from django.contrib.auth import REDIRECT_FIELD_NAME
 
 class PriceView(     
@@ -43,3 +44,26 @@ class PriceView(
 
     success_url = reverse_lazy("events:event-update")
     success_message = _("Priced")
+
+    form_class = PriceForm
+    template_name = "fiestaforms/pages/card_page_for_ajax_form.html"
+    ajax_template_name = "events/price_form.html"
+
+    success_url = reverse_lazy("events:event-detail")
+    success_message = _("Price created sucessfully")
+
+    extra_context = {
+        "form_url": reverse_lazy("events:price"),
+        # "base_page_template": "fiesta/base-variants/center-card-lg.html",
+    }
+
+    @transaction.atomic
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        PriceVariant.objects.get_or_create(
+            event=self.request.event,
+            type=form.instance.type,
+        )
+
+        return response
