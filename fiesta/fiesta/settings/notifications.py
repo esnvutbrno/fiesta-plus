672a49c3c8d0f4af05ee6f8da19783e0dda5d548
Josef Kolář <thejoeejoee@gmail.com>
2023-11-21 18:04:55 +0100

    feat(mailing): enabled smtp mailer III

--- fiesta/fiesta/settings/notifications.py
+++ fiesta/fiesta/settings/notifications.py
@@ -8,6 +8,7 @@ from ._utils import BaseConfigurationProtocol
 class SmtpMailerConfigMixin(BaseConfigurationProtocol):
     MAILER_PRIMARY_BACKEND = Value(default="django.core.mail.backends.smtp.EmailBackend")
     MAILER_PRIMARY_TIMEOUT = Value(default=5)
+    MAILER_PRIMARY_HOST_USE_TLS = Value(default=True)
     MAILER_PRIMARY_HOST = SecretValue()
     MAILER_PRIMARY_HOST_PORT = SecretValue()
     MAILER_PRIMARY_HOST_PASSWORD = SecretValue()
@@ -36,3 +37,7 @@ class SmtpMailerConfigMixin(BaseConfigurationProtocol):
     @property
     def EMAIL_HOST_PASSWORD(self):
         return self.MAILER_PRIMARY_HOST_PASSWORD
+
+    @property
+    def EMAIL_USE_TLS(self):
+        return self.MAILER_PRIMARY_HOST_USE_TLS
