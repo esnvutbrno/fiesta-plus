e2f79033aac0928f799a8db56aaba4919ceff03a
deawer234 <deawer23@seznam.cz>
2024-09-15 11:52:21 +0200

    fix(buddies): prefatch buddy user, uni and faculty

--- fiesta/apps/buddy_system/views/matches.py
+++ fiesta/apps/buddy_system/views/matches.py
@@ -2,6 +2,9 @@ from __future__ import annotations
 
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import ListView
+from django.db.models import Prefetch
+from apps.accounts.models import UserProfile
+from apps.accounts.models.user import User
 
 from apps.fiestarequests.models.request import BaseRequestProtocol
 from apps.plugins.middleware.plugin import HttpRequest
@@ -17,10 +20,13 @@ class MyBuddies(EnsureLocalUserViewMixin, ListView):
     template_name = "buddy_system/my_buddies.html"
 
     def get_queryset(self):
+        user_profile_prefetch = Prefetch(
+            'request__issuer__profile',
+            queryset=UserProfile.objects.select_related('user', 'university', 'faculty')
+        )
         return (
-            self.request.user.buddy_system_request_matches.prefetch_related("request__issuer__profile")
-            .select_related("request", "matcher")
+            self.request.user.buddy_system_request_matches.select_related('request__issuer').prefetch_related(user_profile_prefetch)
             .filter(
-                request__state=BaseRequestProtocol.State.MATCHED,
+                request__state=BaseRequestProtocol.State.MATCHED
             )
         )
