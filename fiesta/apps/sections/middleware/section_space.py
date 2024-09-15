1daee3247dbdac4eac320a82f293bdb65a83a3ab
deawer234 <deawer23@seznam.cz>
2024-09-15 13:29:08 +0200

    feat(sections): adds cache for section space

--- fiesta/apps/sections/middleware/section_space.py
+++ fiesta/apps/sections/middleware/section_space.py
@@ -4,6 +4,9 @@ from django.conf import settings
 from django.contrib.sites.models import Site
 from django.contrib.sites.shortcuts import get_current_site
 from django.http import Http404, HttpResponse
+from django.core.cache import cache
+from django.db.models.signals import post_delete, post_save
+from django.dispatch import receiver
 
 from ...utils.models.query import get_single_object_or_none
 from ...utils.request import HttpRequest as BaseHttpRequest
@@ -19,17 +22,23 @@ class SectionSpaceMiddleware:
         self.get_response = get_response
 
     def __call__(self, request: HttpRequest) -> HttpResponse:
-        # xxx.fiestdomain.tld
         requested_host: str = request.get_host()
         site: Site = get_current_site(request=request)
-
-        # 'xxx' or empty string
         space_slug = requested_host.removesuffix(site.domain).removesuffix(".")
 
-        request.in_space_of_section = get_single_object_or_none(
-            Section.objects.prefetch_plugins(),
-            space_slug=space_slug,
-        )
+        cache_key = f"section_space_{space_slug}"
+        section = cache.get(cache_key)
+        
+        print(section)
+        if section is None:
+            print("Heheheh")
+            section = get_single_object_or_none(
+                Section.objects.prefetch_plugins(),
+                space_slug=space_slug,
+            )
+            cache.set(cache_key, section)
+
+        request.in_space_of_section = section
 
         if not space_slug or space_slug == settings.ROOT_DOMAIN:
             return self.get_response(request)
@@ -37,10 +46,13 @@ class SectionSpaceMiddleware:
         if not request.in_space_of_section:
             raise Http404("Section space not found.")
 
-        if request.in_space_of_section.system_state != Section.SystemState.ENABLED:
-            raise Http404("Section is not enabled.")
-
         return self.get_response(request)
 
+@receiver(post_save, sender=Section)
+@receiver(post_delete, sender=Section)
+def invalidate_section_cache(sender, instance, **kwargs):
+    print("cache invalidation")
+    cache_key = f"section_space_{instance.space_slug}"
+    cache.delete(cache_key)
 
 __all__ = ["SectionSpaceMiddleware", "HttpRequest"]
